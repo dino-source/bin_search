e50262fa01da63a87d88597c3fee1f3ae6cbb134
Dino Source <dinosource.cpp@gmail.com>
2024-03-27 19:47:35 -0700

    Initial commit

---
+++ src/solution.cpp
@@ -0,0 +1,5 @@
+#include "solution.h"
+
+std::optional<int> Solution::bin_search(std::vector<int> arr, int item) {
+    // your code goes here
+}
---
+++ src/solution.h
@@ -0,0 +1,12 @@
+#ifndef DINO_SOURCE_BIN_SEARCH_H
+#define DINO_SOURCE_BIN_SEARCH_H
+
+#include <vector>
+
+
+class Solution {
+public:
+    std::optional<int> bin_search(std::vector<int> arr, int item);
+};
+
+#endif // DINO_SOURCE_BIN_SEARCH_H
