aa3e39db6ea1e9c5d72cfca75a235a02679b70d4
Dino Source <dinosource.cpp@gmail.com>
2024-04-26 17:56:04 -0700

    Fix paths in x-scripts

--- xapp.sh
+++ xapp.sh
@@ -1,4 +1,4 @@
 # You have to define DEVELOP variable as a path to your development 
 # directory in your .bashrc (or .zshrc if you use zsh) file
 
-#DEVELOP/bin_search/build/src/app
+$DEVELOP/bin_search/build/src/app
